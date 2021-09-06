33252d3a834d5293dc843daaee0f8b7ba761c42f
xsaven <gms.sw.xsaven@gmail.com>
2021-09-06 22:30:34 +0300

    app/Jax/User.php

--- app/Jax/User.php
+++ app/Jax/User.php
@@ -91,26 +91,26 @@ class User extends JaxExecutor
 
             $img->orientate();
 
-            if ($img->height() > $img->width()) {
-                $img->resize(168, null, function ($constraint) {
-                    $constraint->aspectRatio();
-                });
-            } else if ($img->height() < $img->width()) {
-                $img->resize(null, 168, function ($constraint) {
-                    $constraint->aspectRatio();
-                });
-            } else {
-                $img->resize(168, 168, function ($constraint) {
-                    $constraint->aspectRatio();
-                });
-            }
-
-            $img = $img->encode('jpg');
-
-
-//            $img = $img->resize(336, 336, function ($constraint) {
+//            if ($img->height() > $img->width()) {
+//                $img->resize(168, null, function ($constraint) {
 //                    $constraint->aspectRatio();
-//                })->encode('jpg');
+//                });
+//            } else if ($img->height() < $img->width()) {
+//                $img->resize(null, 168, function ($constraint) {
+//                    $constraint->aspectRatio();
+//                });
+//            } else {
+//                $img->resize(168, 168, function ($constraint) {
+//                    $constraint->aspectRatio();
+//                });
+//            }
+//
+//            $img = $img->encode('jpg');
+
+
+            $img = $img->resize(336, 336, function ($constraint) {
+                    $constraint->aspectRatio();
+                })->encode('jpg');
 
             $file_name = \Auth::id().'_avatar.jpg';
 
