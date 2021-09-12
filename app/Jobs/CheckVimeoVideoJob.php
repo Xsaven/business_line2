d5f1ad54009050b0e3f45ed1d92cb5590b8f651a
xsaven <gms.sw.xsaven@gmail.com>
2021-09-12 23:56:50 +0300

    app/Jobs/CheckVimeoVideoJob.php

--- app/Jobs/CheckVimeoVideoJob.php
+++ app/Jobs/CheckVimeoVideoJob.php
@@ -37,24 +37,31 @@ class CheckVimeoVideoJob implements ShouldQueue
      */
     public function handle()
     {
-        if ($this->done) {
-            $report = TaskReport::whereFile($this->filename)->first();
-            if ($report) {
-                $report->update([
-                    'status' => $report->task->action_type === Task::ACTION_TYPE_AUTO ? TaskReport::STATUS_CHECKED : TaskReport::STATUS_UPLOADED,
-                ]);
-            } else {
-                \Cache::set($this->filename.'.status', 1, now()->addDay());
-            }
-        }
+//        if ($this->done) {
+//            $report = TaskReport::whereFile($this->filename)->first();
+//            if ($report) {
+//                $report->update([
+//                    'status' => $report->task->action_type === Task::ACTION_TYPE_AUTO ? TaskReport::STATUS_CHECKED : TaskReport::STATUS_UPLOADED,
+//                ]);
+//            } else {
+//                \Cache::set($this->filename.'.status', 1, now()->addDay());
+//            }
+//        }
 
         $lib = new \Vimeo\Vimeo(config('services.vimeo.client_id'), config('services.vimeo.secret'), config('services.vimeo.access_tocken'));
 
         $response = $lib->request($this->uri.'?fields=transcode.status');
         if ($response['body']['transcode']['status'] === 'complete') {
             $report = TaskReport::whereFile($this->filename)->first();
+//            if ($report) {
+//                static::dispatch($this->filename, $this->uri, true)->delay(now()->addSeconds(30));
+//            }
             if ($report) {
-                static::dispatch($this->filename, $this->uri, true)->delay(now()->addSeconds(30));
+                $report->update([
+                    'status' => $report->task->action_type === Task::ACTION_TYPE_AUTO ? TaskReport::STATUS_CHECKED : TaskReport::STATUS_UPLOADED,
+                ]);
+            } else {
+                \Cache::set($this->filename.'.status', 1, now()->addDay());
             }
         } elseif ($response['body']['transcode']['status'] === 'in_progress' && static::$list[$this->filename] < 50) {
             static::$list[$this->filename] = isset(static::$list[$this->filename]) ? static::$list[$this->filename]+1 : 1;
