4b33597d7d892a98db4830467fcf5534a9a73339
xsaven <gms.sw.xsaven@gmail.com>
2021-09-14 12:58:13 +0300

    app/Layouts/Home.php, public/mix-manifest.json, webpack.mix.js

--- app/Layouts/Home.php
+++ app/Layouts/Home.php
@@ -52,7 +52,7 @@ class Home extends LayoutComponent
         'js/nice-select.js',
         'js/functions.js',
         'js/scripts.js',
-        'js/app.js',
+        'js/app_v2.js',
         'ljs' => [
             'jq',
             'echo',
