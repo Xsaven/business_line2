fbc291864c66047dc03dc3a393a3746f37dc471b
xsaven <gms.sw.xsaven@gmail.com>
2021-10-11 17:52:43 +0300

    app/LteAdmin/Controllers/NotificationsController.php

--- app/LteAdmin/Controllers/NotificationsController.php
+++ app/LteAdmin/Controllers/NotificationsController.php
@@ -39,7 +39,7 @@ class NotificationsController extends Controller
             $table->search->at();
 
             $table->col('Пользователь', function (Notification $notification) {
-                return User::find($notification->notifiable_id)->full_name;
+                return User::find($notification->notifiable_id)?->full_name;
             });
             $table->col('Сообщение', function (Notification $notification) {
                 $d = $notification->data;
